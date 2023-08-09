c752ea58146b520a7d9315cda4bf8111893e1153
Luan Delino <delinoperes@gmail.com>
2023-08-08 22:07:59 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/projetojava/demo/domain/Category.java
+++ src/main/java/com/projetojava/demo/domain/Category.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +23,7 @@ public class Category implements Serializable{
     private Integer id;
     private String nome;
 
+    @JsonManagedReference
     @ManyToMany(mappedBy="categorias")
     private List<Product> produtos = new ArrayList<>();
 
--- src/main/java/com/projetojava/demo/services/CategoryService.java
+++ src/main/java/com/projetojava/demo/services/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.projetojava.demo.domain.Category;
 import com.projetojava.demo.repositories.CategoryRepository;
+import com.projetojava.demo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,7 +17,10 @@ public class CategoryService {
 
     public Category search(Integer id) {
         Optional<Category> obj = _categoryRepo.findById(id);
-        return obj.orElse(null);
+
+        return obj.orElseThrow(
+            () -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id)
+        );
     }
 
 }
